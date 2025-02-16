54fe247b7db666cf5798fd15dfcae8b10080a6ff
Jessica Vaz <168509445+Jessicavazm@users.noreply.github.com>
2025-02-16 21:23:54 +1100

    feat: Project init continued with setting up Server and Index files

---
+++ src/server.js
@@ -0,0 +1,26 @@
+// Here, we configure the application server
+// Adding functionalities
+
+
+// Reason for separation:
+// Don't often need to start the server
+// Can import the app without worrying about starting the app
+
+// Import express
+const express = require('express');
+
+// Make an instance of the express server
+const app = express();
+
+// Start defining routes: instance.verb(url, middleware/ callback)
+// GET localhost: 3000/
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    })
+});
+
+// Export server
+module.exports = {
+    app
+}
