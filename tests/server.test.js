266bffa4d62bb8b844e82cf7694e58faa30acd70
Jessica Vaz <168509445+Jessicavazm@users.noreply.github.com>
2025-02-16 22:10:18 +1100

    feat: Add one more case test for not requiring Auth header for the root route GET /

--- tests/server.test.js
+++ tests/server.test.js
@@ -9,11 +9,19 @@ const request = require('supertest');
 describe('Root route', () => {
     // Syntax: test/it (ack msg, callback)
     test("Server returns a 'Hello World!'' message", async () => {
-        // Make a request to the '/' route
+        // Make a request to the '/' route using Supertest
         // Wait for a response and store it
         const response = await request(app).get("/");
 
         // Check the body of the response to see if it matches the 'Hello World!' string
         expect(response.body.message).toBe("Hello World!");
+        expect(response.statusCode).toBe(200);
+
     });
+
+    test("Server returns a response without Auth header", async () => {
+        const response = await request(app).get("/");
+        console.log(response);
+        expect(response.headers["Authorisation"]).toBeFalsy();
+    })
 });
